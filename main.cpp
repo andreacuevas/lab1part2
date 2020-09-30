95a7fe4b90cd8f50530548e9ab7412ce8a330fd5
Andrea <B00120544@mytudublin.ie>
2020-09-30 18:37:51 +0100

    updated file

--- main.cpp
+++ main.cpp
@@ -1,38 +1,39 @@
 #include <iostream>
 #include <cmath>
+
 using namespace std;
 // Base class
 class Shape
 {
 public:
- // pure virtual function providing interface framework.
- virtual float getArea() = 0;
- void setPIE(float PI)
- {
-     pi = PI;
- }
- void setRADIUS(float rad)
- {
-     radius = rad;
- }
+// pure virtual function providing interface framework.
+    virtual float getArea() = 0;
+    void setPIE(float PI)
+    {
+        p = PI;
+    }
+    void setRADIUS(float rad)
+    {
+        r = rad;
+    }
 protected:
- float pi;
- int radius;
+    float p;
+    int r;
 };
 class Circle: public Shape
 {
 public:
- float getArea()
- {
-    return pow(pi*radius,2);
- }
+    float getArea()
+    {
+        return pow(p*r,2);
+    }
 };
 int main(void)
 {
- Circle Cir;
- Cir.setPIE(3.14);
- Cir.setRADIUS(7);
- // Print the area of the object.
- cout << "Total Area of a Circle: " << Cir.getArea() << endl;
- return 0;
+    Circle Cir;
+    Cir.setPIE(3.14);
+    Cir.setRADIUS(7);
+// Print the area of the object.
+    cout << "Total Area of the Circle: " << Cir.getArea() << endl;
+    return 0;
 }
